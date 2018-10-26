e1ef3309c3dc3f8c54c1c601525a54ce60062c63
eduardoparaja <44499403+eduardoparaja@users.noreply.github.com>
2018-10-26 14:49:34 +0200

    changes in linear search

--- exercises session 9.py
+++ exercises session 9.py
@@ -5,9 +5,9 @@ Created on Fri Oct 26 14:36:29 2018
 @author: edup_
 """
 
-def linear(elem, lst):
+def linear(num, lst):
     for i in range(len(lst)):
-        if elem == lst[i]:
+        if num == lst[i]:
             return i
     
     return None
